7be6c33810e940c65b925342c83c84b6a845fb02
WaheedDurrani <waheedkhan6670@gmail.com>
2023-12-26 12:25:11 +0500

    added test

--- first.py
+++ first.py
@@ -4,3 +4,21 @@ st.write('Hello world of streamlit !')
 
 title = st.text_input('Movie title', 'Life of Brian')
 st.write('The current movie title is', title)
+
+st.button('Hit me')
+
+st.checkbox('Check me out')
+st.radio('Pick one:', ['nose','ear'])
+st.selectbox('Select', [1,2,3])
+st.multiselect('Multiselect', [1,2,3])
+st.slider('Slide me', min_value=0, max_value=10)
+st.select_slider('Slide to select', options=[1,'2'])
+st.text_input('Enter some text')
+st.number_input('Enter a number')
+st.text_area('Area for textual entry')
+st.date_input('Date input')
+st.time_input('Time entry')
+st.file_uploader('File uploader')
+
+st.camera_input("一二三,茄子!")
+st.color_picker('Pick a color')
